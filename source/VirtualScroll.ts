39f7a8cf5731ef66f1390c6f7c0c2bb2f07aab39
Yury Chetyrko <ychetyrko@gmail.com>
2019-11-03 15:02:18 +0300

    syncThumbWithDevice

--- source/VirtualScroll.ts
+++ source/VirtualScroll.ts
@@ -161,7 +161,7 @@ export class VirtualScroll extends State {
   }
 
   @trigger
-  syncCanvasThumbWithDevice(): void {
+  syncThumbWithDevice(): void {
     const d = this.device
     const t = this.canvasThumb
     if (d) {
