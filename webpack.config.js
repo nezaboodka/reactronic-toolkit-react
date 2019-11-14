68a0cbdbd3c0f0e820a52da0daf6b33dc929211f
Yury Chetyrko <ychetyrko@gmail.com>
2019-11-14 17:05:29 +0300

    Code structure

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,9 @@ module.exports = {
       '@reactronic-toolkit-react': path.resolve(__dirname, 'source', 'index.ts'),
       '/m': path.resolve(__dirname, 'demo', 'model'),
       '/v': path.resolve(__dirname, 'demo', 'view'),
+      '/model':  path.resolve(__dirname, 'source', 'model'),
+      '/view':  path.resolve(__dirname, 'source', 'view'),
+      '/tools':  path.resolve(__dirname, 'source', 'tools'),
     },
   },
 
