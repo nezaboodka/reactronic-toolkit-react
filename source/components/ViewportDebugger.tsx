7500ce401d1fd81529f452a1552881ea2aae5554
Yury Chetyrko <ychetyrko@gmail.com>
2019-11-15 15:26:42 +0300

    cosmetics

--- source/components/ViewportDebugger.tsx
+++ source/components/ViewportDebugger.tsx
@@ -16,13 +16,13 @@ export function ViewportDebugger(p: {viewport: Viewport}): JSX.Element {
     const v = p.viewport
     return (
       <div className={css.main}>
-        <AreaRect hint={'All Data'} area={v.allCells} px={v.all} key={`all-${counter}`}
+        <AreaRect hint={'All'} area={v.allCells} px={v.all} key={`all-${counter}`}
           className={css.all} style={place(1, 1, 10, 9)}>
           <AreaRect hint={'Surface'} area={v.surfaceCells} px={v.surface} inner={v.loaded} key={`surface-${counter}`}
             className={css.surface} style={place(2, 2, 9, 9)}>
-            <AreaRect hint={'Loaded Data'} area={v.loadedCells} px={v.loaded} key={`buffer-${counter}`}
+            <AreaRect hint={'Loaded'} area={v.loadedCells} px={v.loaded} key={`buffer-${counter}`}
               className={css.buffer} style={place(2, 2, 9, 9)}>
-              <AreaRect hint={'Visible Data'} area={v.visibleCells} px={v.visible} key={`viewport-${counter}`}
+              <AreaRect hint={'Visible'} area={v.visibleCells} px={v.visible} key={`viewport-${counter}`}
                 className={css.viewport} style={place(3, 3, 8, 8)}>
                 <div style={{height: '1em'}}></div>
               </AreaRect>
