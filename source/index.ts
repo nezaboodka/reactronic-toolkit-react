1d768f672350d6847aed4a56c1f622f2a63ca74d
Yury Chetyrko <ychetyrko@gmail.com>
2019-11-15 09:33:07 +0300

    Cosmetics

--- source/index.ts
+++ source/index.ts
@@ -3,14 +3,14 @@
 // Copyright (C) 2019 Yury Chetyrko <ychetyrko@gmail.com>
 // License: https://raw.githubusercontent.com/nezaboodka/reactronic/master/LICENSE
 
-export * from './lib/Area'
-export * from './lib/GroupEvents'
-export * from './lib/Viewport'
-export * from './lib/UserInput'
-
-export * from '/components/RComponent'
-export * from '/components/DataViewport'
+export * from '/lib/Area'
+export * from '/lib/GroupEvents'
+export * from '/lib/Viewport'
+export * from '/lib/UserInput'
 
 export * from '/tools/reactive'
 export * from '/tools/restyle'
 export * from '/tools/place'
+
+export * from '/components/RComponent'
+export * from '/components/DataViewport'
