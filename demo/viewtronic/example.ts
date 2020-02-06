f5f0496cef452cb0829c3949a0627243895d863c
Yury Chetyrko <ychetyrko@gmail.com>
2020-02-06 22:46:17 +0300

    Measure example

--- demo/viewtronic/example.ts
+++ demo/viewtronic/example.ts
@@ -6,12 +6,19 @@
 import { Action, action, Stateful } from 'reactronic'
 
 import { div, i } from '~/viewtronic/html'
-import { reactive } from '~/viewtronic/impl'
+import { reactive, renderChildren } from '~/viewtronic/impl'
 
-class Model extends Stateful {
-  clicks: number = 0
+export class Model extends Stateful {
   x: number = 0
   y: number = 0
+  clicks: number = 0
+  measure: number = 0
+
+  @action
+  move(x: number, y: number): void {
+    this.x = x
+    this.y = y
+  }
 
   @action
   click(): void {
@@ -19,9 +26,8 @@ class Model extends Stateful {
   }
 
   @action
-  move(x: number, y: number): void {
-    this.x = x
-    this.y = y
+  setMeasure(value: number): void {
+    this.measure = value
   }
 }
 
@@ -29,18 +35,27 @@ const model = Action.run('init', () => new Model())
 
 export function Toolbar(id: string, className: string): void {
   div(id, e => {
+    // Self properties
     e.className = className
     e.style.zIndex = '100'
     e.style.display = 'flex'
     e.style.flexDirection = 'row'
     e.onclick = e => model.click()
     e.onmousemove = e => model.move(e.x, e.y)
+
+    // Children
     ToolbarButton('menu', true, 'las la-menu', 'Menu')
-    div('spring', e => e.style.flexGrow = '1')
+    reactive('spring', 'spring', () => {
+      div('spring', e => {
+        e.style.flexGrow = '1'
+        e.style.textAlign = 'center'
+        e.innerText = `Measure: ${model.measure}`
+      })
+    })
     ToolbarButton('settings', false, 'las la-cog', 'Settings')
     ToolbarButton('close', false, 'las la-times', 'Close')
   })
-  // Same in TSX:
+  // Similar in TSX:
   // <div id={id} className={className}>
   //   <ToolbarButton id="menu" icon="las la-menu" caption="Menu"/>
   //   <div id="spring" style={{flexGrow: 1}}/>
@@ -52,11 +67,11 @@ export function Toolbar(id: string, className: string): void {
 
 export function ToolbarButton(id: string, mouse: boolean, icon: string, caption?: string): void {
   reactive(id, ToolbarButton.name, () => {
-    // let measure: HTMLDivElement
-
+    let measure: HTMLDivElement
     div(id, e => {
       e.className = 'fancy-button'
       e.style.margin = '1em'
+      e.style.border = '0.5px dashed gray'
 
       div('icon', e => {
         e.className = 'fancy-button-icon'
@@ -66,11 +81,11 @@ export function ToolbarButton(id: string, mouse: boolean, icon: string, caption?
       div('text', e => {
         e.className = 'fancy-button-text'
         e.innerText = mouse ? `${caption || ''} ${model.clicks} : ${model.x}, ${model.y}` : `${caption || ''} ${model.clicks}`
-        // measure = e
+        measure = e
       })
     })
-
-    // renderChildren() // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    // console.log(`measured: ${measure!.clientHeight}`)
+    renderChildren()
+    if (mouse) // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      model.setMeasure(measure!.clientWidth)
   })
 }
