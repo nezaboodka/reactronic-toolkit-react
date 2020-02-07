3e5fc03e171eb2fd21d8f01a5bbe45f150d27c70
Yury Chetyrko <ychetyrko@gmail.com>
2020-02-07 17:24:03 +0300

    Added button element

--- demo/viewtronic/html.ts
+++ demo/viewtronic/html.ts
@@ -10,31 +10,45 @@ export function div(id: string, render: Render<HTMLDivElement>): void {
   define(id, render, HtmlRtti.div)
 }
 
-export function staticDiv(id: string, render: Render<HTMLDivElement>): void {
-  define(id, render, HtmlRtti.StaticDiv)
+export function button(id: string, render: Render<HTMLButtonElement>): void {
+  define(id, render, HtmlRtti.button)
 }
 
 export function span(id: string, render: Render<HTMLSpanElement>): void {
   define(id, render, HtmlRtti.span)
 }
 
-export function staticSpan(id: string, render: Render<HTMLSpanElement>): void {
-  define(id, render, HtmlRtti.StaticSpan)
-}
-
 export function italic(id: string, render: Render<HTMLSpanElement>): void {
   define(id, render, HtmlRtti.italic)
+}
+
+// Static (non-reactive)
+
+export function staticDiv(id: string, render: Render<HTMLDivElement>): void {
+  define(id, render, HtmlRtti.StaticDiv)
+}
+
+export function staticButton(id: string, render: Render<HTMLButtonElement>): void {
+  define(id, render, HtmlRtti.StaticButton)
+}
+
+export function staticSpan(id: string, render: Render<HTMLSpanElement>): void {
+  define(id, render, HtmlRtti.StaticSpan)
 }
 
 export function staticItalic(id: string, render: Render<HTMLSpanElement>): void {
   define(id, render, HtmlRtti.StaticItalic)
 }
 
+// Run-Time Type Information
+
 const HtmlRtti = {
   div: new HtmlNodeRtti<HTMLDivElement>('div', true),
+  button: new HtmlNodeRtti<HTMLButtonElement>('button', true),
   span: new HtmlNodeRtti<HTMLSpanElement>('span', true),
   italic: new HtmlNodeRtti<HTMLSpanElement>('i', true),
   StaticDiv: new HtmlNodeRtti<HTMLDivElement>('div', false),
+  StaticButton: new HtmlNodeRtti<HTMLButtonElement>('button', false),
   StaticSpan: new HtmlNodeRtti<HTMLSpanElement>('span', false),
   StaticItalic: new HtmlNodeRtti<HTMLSpanElement>('i', false),
 }
