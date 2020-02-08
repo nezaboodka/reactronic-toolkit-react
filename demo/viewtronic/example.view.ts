6cb13e85efa651689aa38dcbc3402db1bc0fa549
Yury Chetyrko <ychetyrko@gmail.com>
2020-02-08 17:59:28 +0300

    Cosmetics

--- demo/viewtronic/example.view.ts
+++ demo/viewtronic/example.view.ts
@@ -11,7 +11,8 @@ import { div } from '~/viewtronic/html'
 
 export function App(id: string, m: Model): void {
   div(id, e => {
-    e.className = style.css.app
+    const cn = style.css
+    e.className = cn.app
     e.onclick = event => m.click()
     e.onmousemove = event => m.move(event.x, event.y)
     Toolbar('toolbar', m)
@@ -20,24 +21,18 @@ export function App(id: string, m: Model): void {
 
 export function Toolbar(id: string, m: Model): void {
   div(id, e => {
-    e.className = style.css.toolbar
-    // div('(mouse)', e => {
-    //   e.className = style.classes.toolbarItem
-    //   e.style.border = 'none'
-    //   e.style.borderBottom = '0.5px solid white'
-    //   e.innerText = `Mouse: ${m.x}, ${m.y} (${m.clicks})`
-    //   m.setSize(e.clientWidth)
-    // })
+    const cn = style.css
+    e.className = cn.toolbar
     if (m.x > 100 && m.y > 100)
       div('pointer', e => {
-        e.className = style.css.pointer
+        e.className = cn.pointer
         e.style.left = `${m.x}px`
         e.style.top = `${m.y}px`
         e.innerText = `(${m.x}, ${m.y})`
         m.setSize(e.clientWidth)
       })
     div('space', e => {
-      e.className = style.css.toolbarItem
+      e.className = cn.toolbarItem
       e.style.border = 'none'
       e.style.flexGrow = '1'
       e.style.textAlign = 'right'
@@ -50,10 +45,11 @@ export function Toolbar(id: string, m: Model): void {
 
 export function ToolbarButton(id: string, icon: string): void {
   div(id, e => {
-    e.className = style.css.toolbarItem
+    const cn = style.css
+    e.className = cn.toolbarItem
     div('icon', e => e.className = cx(icon, style.css.toolbarIcon))
     div('text', e => {
-      e.className = style.css.toolbarText
+      e.className = cn.toolbarText
       e.innerText = `${id}`
     })
   })
